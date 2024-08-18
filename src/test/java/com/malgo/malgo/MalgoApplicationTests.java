d5b167436b301e67f7c278858c8ee09357652e40
malgoo.dev <malgoo.dev@gmail.com>
2024-08-19 01:04:50 +0700

    +init project

---
+++ src/test/java/com/malgo/malgo/MalgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malgo.malgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
